7c8b7c7863837ea0391bad401f3eacd503b9a869
Nicolas Milliard <contact@nicolasmilliard.fr>
2023-07-06 18:58:39 +0200

    Add a RegExp

--- src/application/utils/hours/formatHour.ts
+++ src/application/utils/hours/formatHour.ts
@@ -1,9 +1,10 @@
 const formatHour = (timeString: string) => {
     const date = new Date(timeString);
-    const formattedHour = date.toLocaleTimeString("fr-FR", {
+    const timeStringToHour = date.toLocaleTimeString("fr-FR", {
         hour: "2-digit",
         minute: "2-digit",
     });
+    const formattedHour = timeStringToHour.replace(new RegExp(':', 'g'), 'H');
     return formattedHour;
 };
 
