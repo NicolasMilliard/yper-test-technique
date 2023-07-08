e7c4df753926c5a4b396d87649876646ad983c24
Nicolas Milliard <contact@nicolasmilliard.fr>
2023-07-08 13:10:28 +0200

    Add Map component

--- src/infrastructure/components/SearchResults/SearchResults.tsx
+++ src/infrastructure/components/SearchResults/SearchResults.tsx
@@ -1,36 +1,53 @@
 import { FC } from "react";
-import Container from "react-bootstrap/Container";
 import { Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Map from "../Maps/Map/Map";
 
 import styles from "./SearchResults.module.scss";
 
 interface Result {
   _id: string;
   name: string;
+  address: {
+    location: {
+      coordinates: number[];
+    };
+  };
 }
 
 interface Props {
   results: Result[];
+  latitude: number;
+  longitude: number;
 }
 
-const SearchResults: FC<Props> = ({ results }) => {
+const SearchResults: FC<Props> = ({ results, latitude, longitude }) => {
   return (
     <Container className={styles.container}>
-      <h2 className={styles.title}>Résultats de la recherche&nbsp;:</h2>
-      <ul className={styles.list}>
-        <hr />
-        {results.map((item) => (
-          <span key={item._id}>
-            <li className={styles.listItem}>
-              <h3 className={styles.shopTitle}>{item.name}</h3>
-              <Link className={styles.shopLink} to={`/shop/${item._id}`}>
-                Voir plus d'infos
-              </Link>
-            </li>
+      <Row>
+        <Col sm={12} md={6}>
+          <h2 className={styles.title}>Résultats de la recherche&nbsp;:</h2>
+          <ul className={styles.list}>
             <hr />
-          </span>
-        ))}
-      </ul>
+            {results.map((item) => (
+              <span key={item._id}>
+                <li className={styles.listItem}>
+                  <h3 className={styles.shopTitle}>{item.name}</h3>
+                  <Link className={styles.shopLink} to={`/shop/${item._id}`}>
+                    Voir plus d'infos
+                  </Link>
+                </li>
+                <hr />
+              </span>
+            ))}
+          </ul>
+        </Col>
+        <Col sm={12} md={6}>
+          <Map latitude={latitude} longitude={longitude} zoom={12} markers={results} />
+        </Col>
+      </Row>
     </Container>
   );
 };
--- src/presentation/containers/Shop/ShopDetails/ShopDetails.tsx
+++ src/presentation/containers/Shop/ShopDetails/ShopDetails.tsx
@@ -1,6 +1,9 @@
 import { FC } from "react";
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import MainTitle from "../../../../infrastructure/components/TextFields/MainTitle/MainTitle";
+import Map from "../../../../infrastructure/components/Maps/Map/Map";
 
 import styles from "./ShopDetails.module.scss";
 
@@ -9,18 +12,49 @@ interface Props {
   address: string;
   city: string;
   zipCode: number;
+  latitude: number;
+  longitude: number;
 }
 
-const ShopDetails: FC<Props> = ({ shopName, address, city, zipCode }) => {
+interface Result {
+  _id: string;
+  name: string;
+  address: {
+    location: {
+      coordinates: number[];
+    };
+  };
+}
+
+const ShopDetails: FC<Props> = ({ shopName, address, city, zipCode, latitude, longitude }) => {
+  const marker: Result[] = [
+    {
+      _id: "1",
+      name: shopName,
+      address: {
+        location: {
+          coordinates: [latitude, longitude],
+        },
+      },
+    },
+  ];
+
   return (
     <Container className={styles.container}>
-      <MainTitle text={shopName} />
-      <ul className={styles.list}>
-        <li>{address}</li>
-        <li>
-          {zipCode} {city}
-        </li>
-      </ul>
+      <Row>
+        <Col sm={12} md={6}>
+          <MainTitle text={shopName} />
+          <ul className={styles.list}>
+            <li>{address}</li>
+            <li>
+              {zipCode} {city}
+            </li>
+          </ul>
+        </Col>
+        <Col sm={12} md={6}>
+          <Map latitude={latitude} longitude={longitude} zoom={16} markers={marker} />
+        </Col>
+      </Row>
     </Container>
   );
 };
