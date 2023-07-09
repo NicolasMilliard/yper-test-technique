1efe7ddc182a4583b9b67c0d7fbf5d82a8e31887
Nicolas Milliard <contact@nicolasmilliard.fr>
2023-07-09 13:08:15 +0200

    Remove logic functions

--- src/presentation/containers/RetailPoint/RetailPointContainer/RetailPointContainer.tsx
+++ src/presentation/containers/RetailPoint/RetailPointContainer/RetailPointContainer.tsx
@@ -1,67 +1,12 @@
-import { FC, useState, useEffect, useTransition } from "react";
-import RetailPointDetails from "../RetailPointDetails/RetailPointDetails";
-import DeliveryHours, { Hour } from "../DeliveryHours/DeliveryHours";
-import getShopDetails from "infrastructure/api/Yper/getShopDetails";
+import { FC } from "react";
+import HandleGetRetailPointDetails from "infrastructure/components/SearchRetailPointDetails/HandleGetRetailPointDetails/HandleGetRetailPointDetails";
 
 interface Props {
-  idRetailPoint: string | undefined;
-}
-
-interface ShopDetailsType {
-  name: string;
-  address: {
-    street: string;
-    city: string;
-    zip: number;
-    location: {
-      coordinates: {
-        0: number;
-        1: number;
-      };
-    };
-  };
-  delivery_hours: Hour[];
+  idRetailPoint: string;
 }
 
 const RetailPointContainer: FC<Props> = ({ idRetailPoint }) => {
-  const [isPending, startTransition] = useTransition();
-  const [data, setData] = useState<ShopDetailsType | null>(null);
-
-  useEffect(() => {
-    fetchData();
-  }, [idRetailPoint]);
-
-  const fetchData = async () => {
-    if (idRetailPoint) {
-      try {
-        const response = await getShopDetails(idRetailPoint);
-        // Update state as a non-blocking transition
-        startTransition(() => {
-          setData(response.result);
-        });
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  };
-
-  return (
-    <>
-      {data && (
-        <>
-          <RetailPointDetails
-            shopName={data.name}
-            address={data.address.street}
-            city={data.address.city}
-            zipCode={data.address.zip}
-            latitude={data.address.location.coordinates[1]}
-            longitude={data.address.location.coordinates[0]}
-          />
-          <DeliveryHours hours={data.delivery_hours} />
-        </>
-      )}
-    </>
-  );
+  return <HandleGetRetailPointDetails idRetailPoint={idRetailPoint} />;
 };
 
 export default RetailPointContainer;
