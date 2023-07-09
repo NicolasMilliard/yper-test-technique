f3b2836232b20f224ca18a05b7b081f809f82eaf
Nicolas Milliard <contact@nicolasmilliard.fr>
2023-07-09 11:47:05 +0200

    Better error management

--- src/infrastructure/api/Yper/getRetailPoints.ts
+++ src/infrastructure/api/Yper/getRetailPoints.ts
@@ -1,23 +1,26 @@
 import axios from "axios";
 
 const getRetailPoints = async (lat: number, lng: number) => {
-    const YPER_API_KEY = process.env.REACT_APP_YPER_API_KEY;
-    const max_distance = "30000";
-    const max_results= 10;
+  const YPER_API_KEY = process.env.REACT_APP_YPER_API_KEY;
+  const max_distance = "30000";
+  const max_results = 10;
 
-    try {
-        const timestamp = Date.now();
-        const response = await axios.get(`https://io.beta.yper.org/retailpoint/search?location=${lat},${lng}&max_distance=${max_distance}&limit=${max_results}`, {
-            headers: {
-                Authorization: YPER_API_KEY,
-                "X-Request-Timestamp": timestamp,
-              },
-        });
-        
-        return response.data;
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    const timestamp = Date.now();
+    const response = await axios.get(
+      `https://io.beta.yper.org/retailpoint/search?location=${lat},${lng}&max_distance=${max_distance}&limit=${max_results}`,
+      {
+        headers: {
+          Authorization: YPER_API_KEY,
+          "X-Request-Timestamp": timestamp,
+        },
+      }
+    );
+
+    return response.data;
+  } catch (error) {
+    throw new Error("Echec du chargement des points de vente. Veuillez ré-essayer.");
+  }
+};
 
 export default getRetailPoints;
