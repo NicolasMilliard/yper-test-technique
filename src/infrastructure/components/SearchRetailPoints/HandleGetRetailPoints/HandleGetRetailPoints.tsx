2f9b0cb68b2556a5f586344ecb1df89c9216611f
Nicolas Milliard <contact@nicolasmilliard.fr>
2023-07-20 12:33:31 +0200

    Dynamic update regarding filters

--- src/infrastructure/components/SearchRetailPoints/HandleGetRetailPoints/HandleGetRetailPoints.tsx
+++ src/infrastructure/components/SearchRetailPoints/HandleGetRetailPoints/HandleGetRetailPoints.tsx
@@ -5,6 +5,7 @@ import getRetailPoints from "infrastructure/api/Yper/getRetailPoints";
 import { useSelector, useDispatch } from "react-redux";
 import { setSearch } from "application/redux/searchSlice";
 import { setResults } from "application/redux/resultsSlice";
+import { setPagination } from "application/redux/searchFiltersSlice";
 import { RootState } from "application/redux/store";
 // Components
 import DefaultSearchResults from "presentation/components/SearchRetailsPoints/DefaultSearchResults/DefaultSearchResults";
@@ -24,20 +25,43 @@ const HandleGetRetailPoints: FC<Props> = ({ lat, lng }) => {
   const dispatch = useDispatch();
   const isSearching = useSelector(({ search }: RootState) => search.isSearching);
   const results = useSelector(({ results }: RootState) => results);
-  const pagination = useSelector(({ pagination }: RootState) => pagination.pagination);
+  const pagination = useSelector(({ filters }: RootState) => filters.pagination);
+  const distance = useSelector(({ filters }: RootState) => filters.distance);
   const [isPending, startTransition] = useTransition();
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [error, setError] = useState<string>("");
   const [data, setData] = useState<SearchResultsInterface[]>([]);
+  const [prevDistance, setPrevDistance] = useState<number>(distance);
+  const [prevLat, setPrevLat] = useState<number>(lat);
+  const [prevLng, setPrevLng] = useState<number>(lng);
   const itemsPerPage: number = 10;
 
+  // Used when user navigates through the results (same search)
   useEffect(() => {
+    // New search
     if (isSearching) {
-      fetchRetailPoints(itemsPerPage);
-      // Search is complete
-      dispatch(setSearch(false));
+      // At least one filter has been updated
+      if (distance !== prevDistance || lat !== prevLat || lng !== prevLng) {
+        // Reset cache and filters
+        setData([]);
+        dispatch(setResults([]));
+        dispatch(setPagination(0));
+        setPrevLat(lat);
+        setPrevLng(lng);
+        // API call
+        fetchRetailPoints(itemsPerPage);
+        // Search is complete
+        setPrevDistance(distance);
+        // Search is complete
+        dispatch(setSearch(false));
+      } else if (distance === prevDistance && lat === prevLat && lng === prevLng) {
+        // API or cache call
+        fetchRetailPoints(itemsPerPage);
+        // Search is complete
+        dispatch(setSearch(false));
+      }
     }
-  }, [isSearching, pagination]);
+  }, [isSearching, distance, lat, lng]);
 
   const fetchRetailPoints = async (itemsPerPage: number) => {
     try {
@@ -54,7 +78,7 @@ const HandleGetRetailPoints: FC<Props> = ({ lat, lng }) => {
           setData(cacheResults);
         });
       } else {
-        const response = await getRetailPoints(lat, lng, pagination);
+        const response = await getRetailPoints(lat, lng, pagination, distance);
         // Update state as a non-blocking transition
         startTransition(() => {
           setData(response.result);
