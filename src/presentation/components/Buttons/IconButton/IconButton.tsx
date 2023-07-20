ed0c02291a3234b9b5e7f51ba6bbd1a2cabb2f98
Nicolas Milliard <contact@nicolasmilliard.fr>
2023-07-20 12:34:11 +0200

    Remove text props

--- src/presentation/components/Buttons/IconButton/IconButton.tsx
+++ src/presentation/components/Buttons/IconButton/IconButton.tsx
@@ -3,11 +3,10 @@ import styles from "./IconButton.module.scss";
 // Types
 import { IconButtonInterface } from "application/types/UI";
 
-const IconButton: FC<IconButtonInterface> = ({ imageSource, imageAlt, text, customFunc }) => {
+const IconButton: FC<IconButtonInterface> = ({ imageSource, imageAlt, customFunc }) => {
   return (
     <button onClick={customFunc} className={styles.button}>
       <img src={imageSource} alt={imageAlt} />
-      <span className={styles.text}>{text}</span>
     </button>
   );
 };
