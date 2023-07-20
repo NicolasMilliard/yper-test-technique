77337d379f839f698594b7eccf2ffed7fac3a92c
Nicolas Milliard <contact@nicolasmilliard.fr>
2023-07-20 12:31:27 +0200

    Add TextIconButtonInterface export

--- src/application/types/UI/index.ts
+++ src/application/types/UI/index.ts
@@ -1,3 +1,4 @@
 export type { default as IconButtonInterface } from "./IconButton.interface";
+export type { default as TextIconButtonInterface } from "./TextIconButton.interface";
 export type { default as InputInterface } from "./Input.interface";
 export type { default as SubmitInputInterface } from "./SubmitInput.interface";
