d57a77e85f74c0aba9b7c58557e7cd5a183fbf58
Nicolas Milliard <contact@nicolasmilliard.fr>
2023-07-20 12:32:53 +0200

    Handle filters popup isDisplayed status

--- src/infrastructure/components/Inputs/SubmitLocation/SubmitLocation.tsx
+++ src/infrastructure/components/Inputs/SubmitLocation/SubmitLocation.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "application/redux/store";
 import { setSearch } from "application/redux/searchSlice";
+import { setIsDisplayed } from "application/redux/searchFiltersSlice";
 import SubmitInput from "presentation/components/Inputs/SubmitInput/SubmitInput";
 
 interface Props {
@@ -16,6 +17,7 @@ const SubmitLocation: FC<Props> = ({ value }) => {
     // Check if location is not empty
     if (location.lat !== 0 && location.lng !== 0) {
       dispatch(setSearch(true));
+      dispatch(setIsDisplayed(false));
     } else {
       dispatch(setSearch(false));
     }
