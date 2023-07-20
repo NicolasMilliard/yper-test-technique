38af2e3708517a3b2c4d90cecb4dc96d711bf661
Nicolas Milliard <contact@nicolasmilliard.fr>
2023-07-20 12:37:37 +0200

    Add container

--- src/presentation/containers/RetailPoint/RetailPointContainer/RetailPointContainer.tsx
+++ src/presentation/containers/RetailPoint/RetailPointContainer/RetailPointContainer.tsx
@@ -1,12 +1,18 @@
 import { FC } from "react";
 import HandleGetRetailPointDetails from "infrastructure/components/SearchRetailPointDetails/HandleGetRetailPointDetails/HandleGetRetailPointDetails";
 
+import styles from "./RetailPointContainer.module.scss";
+
 interface Props {
   idRetailPoint: string;
 }
 
 const RetailPointContainer: FC<Props> = ({ idRetailPoint }) => {
-  return <HandleGetRetailPointDetails idRetailPoint={idRetailPoint} />;
+  return (
+    <div className={styles.container}>
+      <HandleGetRetailPointDetails idRetailPoint={idRetailPoint} />
+    </div>
+  );
 };
 
 export default RetailPointContainer;
