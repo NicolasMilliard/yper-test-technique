de5df4012c1a28af49e38dca7d1d788ad4e01a53
Nicolas Milliard <contact@nicolasmilliard.fr>
2023-07-27 15:02:10 +0200

    Add itinerary link

--- src/presentation/containers/RetailPoint/RetailPointDetailsContainer/RetailPointDetailsContainer.tsx
+++ src/presentation/containers/RetailPoint/RetailPointDetailsContainer/RetailPointDetailsContainer.tsx
@@ -3,6 +3,7 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import MainTitle from "presentation/components/TextFields/MainTitle/MainTitle";
+import Link from "presentation/components/Link/Link";
 import Map from "infrastructure/components/Maps/Map/Map";
 import styles from "./RetailPointDetailsContainer.module.scss";
 // Types
@@ -41,6 +42,7 @@ const RetailPointDetailsContainer: FC<Props> = ({ shopName, address, city, zipCo
               {zipCode} {city}
             </li>
           </ul>
+          <Link link={`https://www.google.com/maps/dir//${shopName}`} target="_blank" text="Obtenir l'itinéraire" />
         </Col>
         <Col sm={12} md={6}>
           <Map latitude={latitude} longitude={longitude} zoom={16} mapHeight={311} markers={marker} />
